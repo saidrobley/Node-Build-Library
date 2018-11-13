8dac90a0c9ca806a7e6a2ad748e4c4bc7e4a662e
Said Robley <saidrobley@gmail.com>
2018-11-13 15:16:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var chalk = require("chalk");
+var debug = require("debug")("app");
+var morgan = require("morgan");
+var path = require("path");
+
+var app = express();
+
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dis/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dis/js"))
+);
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(3000, function() {
+  console.log(`listening on port ${chalk.green("3000")}`);
+});
